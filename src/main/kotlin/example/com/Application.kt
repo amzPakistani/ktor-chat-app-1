94cc6ae894cd9a3765cf2034fb9eb8b955126021
Abdul Majid <abdulmajidzeeshan4@gmail.com>
2024-07-20 17:24:46 +0500

    update

--- src/main/kotlin/example/com/Application.kt
+++ src/main/kotlin/example/com/Application.kt
@@ -1,6 +1,9 @@
 package example.com
 
+import com.mongodb.kotlin.client.coroutine.MongoClient
+import example.com.data.MongoMessageDataSource
 import example.com.plugins.*
+import example.com.room.RoomController
 import io.ktor.server.application.*
 
 fun main(args: Array<String>) {
@@ -8,9 +11,13 @@ fun main(args: Array<String>) {
 }
 
 fun Application.module() {
+    val db = MongoClient.create().getDatabase("message_db_1")
+    val messageDataSource = MongoMessageDataSource(db)
+    val roomController = RoomController(messageDataSource)
+
     configureSockets()
     configureSerialization()
     configureMonitoring()
     configureSecurity()
-    configureRouting()
+    configureRouting(roomController)
 }
--- src/main/kotlin/example/com/plugins/Routing.kt
+++ src/main/kotlin/example/com/plugins/Routing.kt
@@ -1,13 +1,15 @@
 package example.com.plugins
 
+import example.com.room.RoomController
+import example.com.routes.chatSocket
+import example.com.routes.getAllMessages
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Application.configureRouting() {
+fun Application.configureRouting(roomController: RoomController) {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+        chatSocket(roomController)
+        getAllMessages(roomController)
     }
 }
